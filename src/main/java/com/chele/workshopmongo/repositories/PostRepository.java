708b1aa36c82c6d5caa69ec0ea5475583b3d9606
Michel Ferreira <michel.mf628@gmail.com>
2024-02-13 20:45:28 -0300

    Projection of author data with DTO

---
+++ src/main/java/com/chele/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.chele.workshopmongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.chele.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{ // o MongoRepository ja tem toda as funçoes CRUD que p´recisamos, prescisamos colocar o tipo da classe de dominio e o tipo do seu id 
+    
+}
