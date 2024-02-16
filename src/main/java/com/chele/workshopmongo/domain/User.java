3d4f51de29579e500413d5c98c53f9beed499272
Michel Ferreira <michel.mf628@gmail.com>
2024-02-16 19:52:40 -0300

    Referencing user posts

--- src/main/java/com/chele/workshopmongo/domain/User.java
+++ src/main/java/com/chele/workshopmongo/domain/User.java
@@ -1,14 +1,19 @@
 package com.chele.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "user")
 public class User implements Serializable {
     private String id;
     private String name;
     private String email;
-
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+    
     public User() {
     }
 
@@ -40,6 +45,14 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
